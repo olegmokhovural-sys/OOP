f0d1ff5ca7a20bce284388d653361612383763ce
Oleg Mokhov <olegmokhovural@gmail.com>
2026-07-25 20:11:02 +0300

    PEP8 src

--- src/category.py
+++ src/category.py
@@ -1,17 +1,17 @@
 from src.product import Product
 
+
 class Category:
     name: str
     description: str
     products: list
-    total_categories = 0
-    total_products = 0
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
 
-        Category.total_categories += 1
-        Category.total_products += len(products)
-
+        Category.category_count += 1
+        Category.product_count += len(products)
