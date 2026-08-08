1cd2cef9f86e4ba9bf08d7b75b4868b273725cf4
Oleg Mokhov <olegmokhovural@gmail.com>
2026-08-08 17:11:41 +0300

    Написаны функции

--- src/category.py
+++ src/category.py
@@ -12,7 +12,24 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        """
+        Метод для добавления товара в категорию.
+        """
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        if not self.__products:
+            return "В категории нет товаров"
+
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return result
--- src/product.py
+++ src/product.py
@@ -2,11 +2,30 @@ class Product:
     '''Создает и инициирует класс'''
     name: str
     description: str
-    price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"]
+        )
